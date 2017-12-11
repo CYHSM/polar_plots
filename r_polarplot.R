794871310124e3633c1967db87ae943098a4b489
IPolti <nachopolti@gmail.com>
2017-12-11 18:00:35 +0100

    Added a comment about the function

--- r_polarplot.R
+++ r_polarplot.R
@@ -1,5 +1,5 @@
 r_polarplot <- function(theta, rho, save) {
-
+# Its very easy with ggplot2 to make a polarplot
 # Data used for example plot 
 # theta <- c(0,0.3927,0.7854,1.1781,1.5708,1.9635,2.3562,2.7489,3.1416,3.5343,3.9270,4.3197,4.7124,5.1051,5.4978,5.8905,6.2832)
 # rho = c(4,9,20,41,66,121,155,181,156,124,76,27,11,4,2,2,1)
