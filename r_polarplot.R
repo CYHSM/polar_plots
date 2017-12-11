52f742fc87e8782fe0288649af4b8ba3ffcd6330
CYHSM <markus.frey1@gmail.com>
2017-12-11 17:43:19 +0100

    Added polar plots for R

---
+++ r_polarplot.R
@@ -0,0 +1,20 @@
+r_polarplot <- function(theta, rho, save) {
+
+# Data used for example plot 
+# theta <- c(0,0.3927,0.7854,1.1781,1.5708,1.9635,2.3562,2.7489,3.1416,3.5343,3.9270,4.3197,4.7124,5.1051,5.4978,5.8905,6.2832)
+# rho = c(4,9,20,41,66,121,155,181,156,124,76,27,11,4,2,2,1)
+
+# Create dataframe for plotting
+polar_df = data.frame(theta, rho)
+
+# Plot with ggplot2
+library(ggplot2)
+ggplot(data=p_df, aes(x=theta, y=rho)) + coord_polar(start=pi/2) + geom_line() + 
+  scale_x_continuous(breaks=seq(0, pi*2-pi/4, by=pi/4), labels=c('0','45','90', '135','180', '225', '270','315')) + 
+  theme_bw() + theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())
+
+if (save){
+  ggsave('r_polarplot.jpg', plot=last_plot())
+}
+
+}
