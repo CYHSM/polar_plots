e8485ef7efe46b0cf88017802ebf0836a0ffa48f
CYHSM <markus.frey1@gmail.com>
2017-12-26 13:30:39 +0100

    Removed weird degree symbol on xkcd plots

--- xkcd_polarplot.py
+++ xkcd_polarplot.py
@@ -5,10 +5,16 @@ plt.xkcd()
 def matplotlib_polarplot(theta, rho, save):
     ax = plt.subplot(111, projection='polar')
     ax.plot(theta, rho)
-    # plt.show()
+
     if save:
         plt.savefig('plots/xkcd_polarplot')
+    else:
+        plt.show()
 
 theta = np.array([0,0.3927,0.7854,1.1781,1.5708,1.9635,2.3562,2.7489,3.1416,3.5343,3.9270,4.3197,4.7124,5.1051,5.4978,5.8905,6.2832])
 rho = np.array([4,9,20,41,66,121,155,181,156,124,76,27,11,4,2,2,1])
+
+# Plot and save to file
 matplotlib_polarplot(theta, rho, True)
+# Just show plot
+# matplotlib_polarplot(theta, rho, False)
